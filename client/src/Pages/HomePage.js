054d62df25747fcc54df9f832ad3427ea86e1fa1
alirangwala <alirbaba40@gmail.com>
2021-05-26 18:44:28 -0600

    Added slideshow to home

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,9 +1,34 @@
-import homepageImage from "../images/mormonTemple.jpeg";
+import image1 from "../images/mormonTemple.jpeg";
+import image2 from "../images/firestone.jpg";
+import image3 from "../images/PBCBank.jpg";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const [current, setCurrent] = useState(0);
+  let images = [image1, image2, image3];
+
+  const switchImage = () => {
+    if (current < images.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      setCurrent(0);
+    }
+    return current;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(switchImage, 3000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="HomePage">
-      <img src={homepageImage} alt="" />
+      <img
+        key={current}
+        className="fade-in-image"
+        src={images[current]}
+        alt=""
+      />
     </div>
   );
 }
